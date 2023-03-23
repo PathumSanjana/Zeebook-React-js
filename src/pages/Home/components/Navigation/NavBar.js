77ca53632a0678f438312788bcd050ec48a2423e
PathumSanjana <silvakp-se19030@stu.kln.ac.lk>
2023-03-24 00:08:50 +0530

    navigation bar added

--- src/pages/Home/components/Navigation/NavBar.js
+++ src/pages/Home/components/Navigation/NavBar.js
@@ -1,7 +1,46 @@
 import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import MessageIcon from '@mui/icons-material/Message';
+import SettingsIcon from '@mui/icons-material/Settings';
+import "./navBar.css"
 
 export default function NavBar() {
   return (
-    <div className="navBarBox"></div>
+    <div className="navBarBox">
+        <div className="navBarLeft">
+            <span className="logo">Zeebook</span>
+        </div>
+
+        <div className="navBarCenter">
+            <div className="searchBarBox">
+            <SearchIcon className='searchIcon'/>
+            <input placeholder='Search for your friend' className="searchInput" />
+            </div>
+        </div>
+        <div className="navBarRight">
+            <div className="navBarLinks">
+                <span className="navBarLink">HomePage</span>
+                <span className="navBarLink">Profile</span>
+            </div>
+            <div className="navBarIcons">
+                <div className="navBarIcon">
+                    <PersonIcon/>
+                   <span className="iconTag">4</span>
+                </div>
+                <div className="navBarIcon">
+                    <MessageIcon/>
+                    <span className="iconTag">6</span>
+                </div>
+                <div className="navBarIcon">
+                    <SettingsIcon/>
+                    <span className="iconTag">3</span>
+                </div>
+            </div>
+            <div className="pic">
+                <img src="/images/1.jpeg" alt="" className="profilePicimg" />
+            </div>
+        </div>
+    </div>
   )
 }
