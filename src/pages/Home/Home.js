16a184d47db95ba1aedefef7ddc93dca13c7460f
PathumSanjana <silvakp-se19030@stu.kln.ac.lk>
2023-03-27 23:45:40 +0530

    Home page basic components added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import LeftPane from './components/LeftPane/LeftPane'
 import NavBar from './components/Navigation/NavBar'
+import PostPane from './components/PostPane/PostPane'
+import RightPane from './components/RightPane/RightPane'
+import "./home.css"
 
 export default function Home() {
   return (
+   <>
    <NavBar/>
+   <div className="bottomContainer">
+    <LeftPane/>
+    <PostPane/>
+    <RightPane/>
+   </div>
+   
+   
+   </>
+   
   )
 }
---
+++ src/pages/Home/components/PostPane/PostPane.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./postPane.css"
+
+export default function PostPane() {
+  return (
+    <div className='postPainBox'>PostPane</div>
+  )
+}
