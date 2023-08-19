2d787fba75ca272d62bcaf78bd6fa7709daec5e4
PathumSanjana <silvakp-se19030@stu.kln.ac.lk>
2023-08-19 23:57:08 +0530

    Post component created

---
+++ src/pages/Home/components/Post/Post.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./post.css"
+
+export default function Post() {
+  return (
+    <div className="post">
+        <div className="postContainer">
+            <div className="postTop"></div>
+            <div className="postCenter"></div>
+            <div className="postBottom"></div>
+        </div>
+    </div>
+  )
+}
--- src/pages/Home/components/PostPane/PostPane.js
+++ src/pages/Home/components/PostPane/PostPane.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./postPane.css"
 import AddPost from '../AddPost/AddPost'
+import Post from '../Post/Post'
 
 export default function PostPane() {
   return (
     <div className='postPainBox'>
       <AddPost/>
+      <Post/>
     </div>
   )
 }
