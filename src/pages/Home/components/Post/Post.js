92edad8544ac567f275c3f889df8d7695d4b7074
PathumSanjana <silvakp-se19030@stu.kln.ac.lk>
2023-08-20 01:37:06 +0530

    post content added and css added

--- src/pages/Home/components/Post/Post.js
+++ src/pages/Home/components/Post/Post.js
@@ -5,9 +5,26 @@ export default function Post() {
   return (
     <div className="post">
         <div className="postContainer">
-            <div className="postTop"></div>
-            <div className="postCenter"></div>
-            <div className="postBottom"></div>
+            <div className="postTop">
+                <img className="postImage" src="/images/2.jpg" alt="" />
+                <span className="postUsername">Pathum Sanjana</span>
+                <span className="postTime">5 mins ago</span>
+            </div>
+            <div className="postCenter">
+                <div className="postCaption">Hey its my first post!!! I like to live with nature.</div>
+                <img className="postedImage" src="/images/2.jpg" alt="" />
+            </div>
+            <div className="postBottom">
+                <div className="postBottomLeft">
+                    <img className="reactionPic" src="/images/like.png" alt="" />
+                    <img className="reactionPic" src="/images/heart.png" alt="" />
+                    <img className="reactionPic" src="/images/haha.png" alt="" />
+                    <span className="likeCount">Superman and 225 other</span>
+                </div>
+                <div className="postBottomRight">
+                    <span className="commentCount">15 comments</span>
+                </div>
+            </div>
         </div>
     </div>
   )
